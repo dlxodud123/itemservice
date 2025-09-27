a5ac9e48552776d4fd0715b69f5ac51a173ba768
LeeTaeYoung <xodud5080@naver.com>
2025-09-27 16:16:14 +0900

    embedded & spring boot

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,14 +2,17 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 
-//@Import(JdbcTemplateV2Config.class)
+@Slf4j
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
@@ -22,6 +25,6 @@ public class ItemServiceApplication {
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
-	} 
+	}
 
 }
